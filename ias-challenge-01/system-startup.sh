852da6fb0d207266f58ece3155ef5bab0c380a11
Vitor Chaves <vitorclcoelho@protonmail.com>
2022-11-02 17:16:40 -0300

    Add code to remove groups at ias-challenge-01 script

--- ias-challenge-01/system-startup.sh
+++ ias-challenge-01/system-startup.sh
@@ -1,24 +1,42 @@
+#!/bin/bash
 
 echo "Cleaning..."
 
 echo "Removing folders..."
 
 if [ -d "/publico" ]; then
-        echo "Removing /publico folder";
-        rmdir /publico;
+    echo "  - Removing /publico folder";
+    rmdir /publico;
 fi
 
 if [ -d "/adm" ]; then
-        echo "Removing /adm folder";
-        rmdir /adm;
+    echo "  - Removing /adm folder";
+    rmdir /adm;
 fi
 
 if [ -d "/ven" ]; then
-        echo "Removing /ven folder";
-        rmdir /ven;
+    echo "  - Removing /ven folder";
+    rmdir /ven;
 fi
 
 if [ -d "/sec" ]; then
-        echo "Removing /sec folder";
-        rmdir /sec;
+    echo "  - Removing /sec folder";
+    rmdir /sec;
+fi
+
+echo "Removing groups..."
+
+if (grep -q -E "GRP_ADM" /etc/group) then
+    echo "  - Removing GRP_ADM group";
+    groupdel GRP_ADM;
+fi
+
+if (grep -q -E "GRP_VEN" /etc/group) then
+    echo "  - Removing GRP_VEN group";
+    groupdel GRP_VEN;
+fi
+
+if (grep -q -E "GRP_SEC" /etc/group) then
+    echo "  - Removing GRP_SEC group";
+    groupdel GRP_SEC;
 fi
