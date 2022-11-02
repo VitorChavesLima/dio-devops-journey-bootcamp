a137d18671c02f20a5f604f6e60f6592c2469ca5
Vitor Chaves <vitorclcoelho@protonmail.com>
2022-11-02 17:23:15 -0300

    Add code to remove users at ias-challenge-01 script

--- ias-challenge-01/system-startup.sh
+++ ias-challenge-01/system-startup.sh
@@ -40,3 +40,50 @@ if (grep -q -E "GRP_SEC" /etc/group) then
     echo "  - Removing GRP_SEC group";
     groupdel GRP_SEC;
 fi
+
+echo "Removing users..."
+
+if (grep -q -E "carlos" /etc/passwd) then
+    echo "  - Removing user: carlos";
+    userdel carlos;
+fi
+
+if (grep -q -E "maria" /etc/passwd) then
+    echo "  - Removing user: maria";
+    userdel maria;
+fi
+
+if (grep -q -E "joao" /etc/passwd) then
+    echo "  - Removing user: joao";
+    userdel joao;
+fi
+
+if (grep -q -E "debora" /etc/passwd) then
+    echo "  - Removing user: debora";
+    userdel debora;
+fi
+
+if (grep -q -E "sebastiana" /etc/passwd) then
+    echo "  - Removing user: sebastiana";
+    userdel sebastiana;
+fi
+
+if (grep -q -E "roberto" /etc/passwd) then
+    echo "  - Removing user: roberto";
+    userdel roberto;
+fi
+
+if (grep -q -E "josefina" /etc/passwd) then
+    echo "  - Removing user: josefina";
+    userdel josefina;
+fi
+
+if (grep -q -E "amanda" /etc/passwd) then
+    echo "  - Removing user: amanda";
+    userdel amanda;
+fi
+
+if (grep -q -E "rogerio" /etc/passwd) then
+    echo "  - Removing user: rogerio";
+    userdel rogerio;
+fi
